f2b47a91c9b4cca574a51a9c5a6f85403a0ca804
naari3 <naari.named@gmail.com>
2021-02-23 16:54:13 +0900

    Revert "Implement trait RAMBus"
    This reverts commit 06fbc01931fa5cc9ac6727d6655ff02d323b9cc6.

--- src/ram.rs
+++ src/ram.rs
@@ -5,56 +5,16 @@ pub struct RAM {
     inner: [u8; MAX_MEMORY],
 }
 
-pub trait RAMBus {
-    fn read_byte(&mut self, address: usize) -> u8 {
-        *self.read_byte_ref(address)
-    }
-    fn read_byte_ref(&self, address: usize) -> &u8;
-    fn write_byte(&mut self, address: usize, byte: u8) {
-        *self.write_byte_ref(address) = byte;
-    }
-    fn write_byte_ref(&mut self, address: usize) -> &mut u8;
-    fn initialize(&mut self);
-}
-
-impl RAMBus for RAM {
-    fn read_byte(&mut self, address: usize) -> u8 {
-        *self.read_byte_ref(address)
-    }
-
-    fn read_byte_ref(&self, address: usize) -> &u8 {
-        &self.inner[address]
-    }
-
-    fn write_byte(&mut self, address: usize, byte: u8) {
-        *self.write_byte_ref(address) = byte;
-    }
-
-    fn write_byte_ref(&mut self, address: usize) -> &mut u8 {
-        &mut self.inner[address]
-    }
-
-    fn initialize(&mut self) {
-        self.inner = [0; MAX_MEMORY];
-    }
-}
-
-impl Index<usize> for RAM
-where
-    RAM: RAMBus,
-{
+impl Index<usize> for RAM {
     type Output = u8;
     fn index(&self, index: usize) -> &Self::Output {
-        self.read_byte_ref(index)
+        &self.inner[index]
     }
 }
 
-impl IndexMut<usize> for RAM
-where
-    RAM: RAMBus,
-{
+impl IndexMut<usize> for RAM {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        self.write_byte_ref(index)
+        &mut self.inner[index]
     }
 }
 
@@ -63,6 +23,25 @@ impl Default for RAM {
         RAM {
             inner: [0; MAX_MEMORY],
         }
+    }
+}
+
+impl RAM {
+    pub fn initialize(&mut self) {
+        self.inner = [0; MAX_MEMORY];
+    }
+
+    pub fn read_byte(&mut self, address: usize) -> u8 {
+        self.inner[address]
+    }
+
+    pub fn write_byte(&mut self, address: usize, byte: u8) {
+        self.inner[address] = byte;
+    }
+
+    #[allow(dead_code)]
+    pub fn write_rom(&mut self, start_address: usize, data: &[u8]) {
+        self.inner[start_address..(start_address + data.len())].clone_from_slice(data);
     }
 }
 
